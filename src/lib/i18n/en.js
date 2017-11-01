d75d2d871a8e75e40db4dc0edc091c79b96b5b04
szlezak <robertszlezak@gmail.com>
2017-11-01 14:13:35 +0100

    Improved info

--- src/lib/i18n/en.js
+++ src/lib/i18n/en.js
@@ -10,40 +10,28 @@ export const en = {
   game: {
     hideResult: 'Hide result',
   },
-  rules: {
-    firstTitle: 'Shake, shake, shake',
-    firstDetailA: 'Shake the phone and hide the result!',
-    firstDetailB: 'Your result should be the same or greater than the previous result.',
-    firstDetailC: 'Say the result without showing the phone (first you say the bigger number and then the smaller one - see the last Chapter - Values).',
-    firstDetailD: 'Click hide, so the next player doesn\'t see the result.',
-    secondTitle: 'Pass the phone to the next player',
-    secondDetailA: 'The next player should say if he trusts you, or he doesnt.',
-    secondDetailB: 'If he doesn\'t click on the \'Show\' button.',
-    secondDetailC: 'If you have been telling the truth, he drinks. Else take a shot you liar!',
-    thirdTitle: 'Values',
-    thirdDetailA: `
-    2-1 "21" Mexicali - Beats all.
-    3-1 "31" Reverse - Rotates play.
-    3-2 "32" Social - Everybody drinks.
-    4-1 "41"
-    4-2 "42"
-    4-3 "43"
-    4-5 "45"
-    5-1 "51"
-    5-2 "52"
-    5-3 "53"
-    5-4 "54"
-    6-1 "61"
-    6-2 "62"
-    6-3 "63"
-    6-4 "64"
-    6-5 "65"
-    1-1 "100"
-    2-2 "200"
-    3-3 "300"
-    4-4 "400"
-    5-5 "500"
-    6-6 "600"`,
-    secondDetailD: 'You may bluff what you rolled in order to avoid drinking. However, the last person to roll before you has the ability to call your bluff. If done successfully, you must drink. If they call your bluff and lose (ie. you really did beat the last score) then that person must drink.',
+  info: {
+    firstSlide: {
+      title: 'Shake, shake, shake',
+      descriptionA: 'Shake the phone and hide the result!',
+      descriptionB: 'Your result should be the same or greater than the previous result.',
+      descriptionC: 'Say the result without showing the phone (first you say the bigger number and then the smaller one - see the last Chapter - Values).',
+      descriptionD: 'Click hide, so the next player doesn\'t see the result.',
+    },
+    secondSlide: {
+      title: 'Pass the phone to the next player',
+      descriptionA: 'The next player should say if he trusts you, or he doesnt.',
+      descriptionB: 'If he doesn\'t click on the \'Show\' button.',
+      descriptionC: 'If you have been telling the truth, he drinks. Else take a shot you liar!',
+      descriptionD: 'You may bluff what you rolled in order to avoid drinking. However, the last person to roll before you has the ability to call your bluff. If done successfully, you must drink. If they call your bluff and lose (ie. you really did beat the last score) then that person must drink.',
+    },
+    thirdSlide: {
+      title: 'Values',
+      descriptionA: '2-1 "21" Mexicali - Beats all.\n3-1 "31" Reverse - Rotates play.\n3-2 "32" Social - Everybody drinks.',
+      descriptionB: '4-1 "41"\n4-2 "42"\n4-3 "43"\n4-5 "45"',
+      descriptionC: '5-1 "51"\n5-2 "52"\n5-3 "53"\n5-4 "54"',
+      descriptionD: '6-1 "61"\n6-2 "62"\n6-3 "63"\n6-4 "64"\n6-5 "65"',
+      descriptionE: '1-1 "100"\n2-2 "200"\n3-3 "300"\n4-4 "400"\n5-5 "500"\n6-6 "600"',
+    },
   },
 };
--- src/screens/Info.js
+++ src/screens/Info.js
@@ -10,50 +10,119 @@ import Swiper from 'react-native-swiper';
 import I18n from 'react-native-i18n';
 
 var styles = StyleSheet.create({
-  slide: {
+  slideWrapper: {
     flex: 1,
     backgroundColor: '#ffffff',
-    paddingHorizontal: 35,
+    paddingHorizontal: 25,
     paddingVertical: 15,
+    justifyContent: 'center',
   },
-  text: {
+  title: {
     color: '#000000',
     fontSize: 30,
     fontWeight: '700',
     textAlign: 'center',
+    fontFamily: 'Roboto',
+    marginBottom: 15,
+  },
+  descriptionWrapper: {
+    flexDirection: 'row',
   },
   description: {
     color: '#000000',
-    textAlign: 'center',
-    fontSize: 18,
+    fontSize: 20,
+    fontFamily: 'Roboto',
+    marginBottom: 5,
+    lineHeight: 24,
   },
-  image: {
-    flex: 1,
+  bullet: {
+    fontSize: 30,
+    marginRight: 10,
+    color: '#000000',
+    lineHeight: 24,
+  },
+  bestCombination: {
+    flexDirection: 'column',
     alignItems: 'center',
-    width: 100,
-    height: 100,
-  }
+    marginBottom: 10,
+  },
+  otherCombination: {
+    flexDirection: 'row',
+  },
+  imageWrapper: {
+    alignItems: 'center',
+    marginBottom: 10,
+  },
+  shakeImage: {
+    width: 120,
+    height: 120,
+  },
 });
 
 class Info extends Component {
   render() {
     return (
       <Swiper style={styles.wrapper} activeDotColor="#673AB7">
-        <View style={styles.slide}>
-          <Text style={styles.text}>{I18n.t('rules.firstTitle')}</Text>
-          <Text style={styles.description}>{I18n.t('rules.firstDetailA')}</Text>
-          <Text style={styles.description}>{I18n.t('rules.firstDetailB')}</Text>
-          <Text style={styles.description}>{I18n.t('rules.firstDetailC')}</Text>
+        <View style={styles.slideWrapper}>
+          <View style={styles.imageWrapper}>
+            <Image
+              style={styles.shakeImage}
+              source={require('../assets/shake.png')}
+              resizeMode='contain'
+            />
+          </View>
+          <Text style={styles.title}>{I18n.t('info.firstSlide.title')}</Text>
+          <View style={styles.descriptionWrapper}>
+            <Text style={styles.bullet}>{'\u2022'}</Text>
+            <Text style={styles.description}>
+              {I18n.t('info.firstSlide.descriptionA')}
+            </Text>
+          </View>
+          <View style={styles.descriptionWrapper}>
+            <Text style={styles.bullet}>{'\u2022'}</Text>
+            <Text style={styles.description}>{I18n.t('info.firstSlide.descriptionB')}</Text>
+          </View>
+          <View style={styles.descriptionWrapper}>
+            <Text style={styles.bullet}>{'\u2022'}</Text>
+            <Text style={styles.description}>{I18n.t('info.firstSlide.descriptionC')}</Text>
+          </View>
+          <View style={styles.descriptionWrapper}>
+            <Text style={styles.bullet}>{'\u2022'}</Text>
+            <Text style={styles.description}>{I18n.t('info.firstSlide.descriptionD')}</Text>
+          </View>
         </View>
-        <View style={styles.slide}>
-          <Text style={styles.text}>{I18n.t('rules.secondTitle')}</Text>
-          <Text style={styles.description}>{I18n.t('rules.secondDetailA')}</Text>
-          <Text style={styles.description}>{I18n.t('rules.secondDetailB')}</Text>
-          <Text style={styles.description}>{I18n.t('rules.secondDetailC')}</Text>
+        <View style={styles.slideWrapper}>
+          <Text style={styles.title}>{I18n.t('info.secondSlide.title')}</Text>
+          <View style={styles.descriptionWrapper}>
+            <Text style={styles.bullet}>{'\u2022'}</Text>
+            <Text style={styles.description}>{I18n.t('info.secondSlide.descriptionA')}</Text>
+          </View>
+          <View style={styles.descriptionWrapper}>
+            <Text style={styles.bullet}>{'\u2022'}</Text>
+            <Text style={styles.description}>{I18n.t('info.secondSlide.descriptionB')}</Text>
+          </View>
+          <View style={styles.descriptionWrapper}>
+            <Text style={styles.bullet}>{'\u2022'}</Text>
+            <Text style={styles.description}>{I18n.t('info.secondSlide.descriptionC')}</Text>
+          </View>
+          <View style={styles.descriptionWrapper}>
+            <Text style={styles.bullet}>{'\u2022'}</Text>
+            <Text style={styles.description}>{I18n.t('info.secondSlide.descriptionD')}</Text>
+          </View>
         </View>
-        <View style={styles.slide}>
-          <Text style={styles.text}>{I18n.t('rules.thirdTitle')}</Text>
-          <Text style={styles.description}>{I18n.t('rules.thirdDetailA')}</Text>
+        <View style={styles.slideWrapper}>
+          <Text style={styles.title}>{I18n.t('info.thirdSlide.title')}</Text>
+          <View>
+            <View style={styles.bestCombination}>
+              <Text style={styles.description}>{I18n.t('info.thirdSlide.descriptionA')}</Text>
+            </View>
+            <View style={styles.otherCombination}>
+              <Text style={[styles.description, {flex: 1}]}>{I18n.t('info.thirdSlide.descriptionB')}</Text>
+              <Text style={[styles.description, {flex: 1}]}>{I18n.t('info.thirdSlide.descriptionC')}</Text>
+              <Text style={[styles.description, {flex: 1}]}>{I18n.t('info.thirdSlide.descriptionD')}</Text>
+              <Text style={[styles.description, {flex: 1}]}>{I18n.t('info.thirdSlide.descriptionE')}</Text>
+            </View>
+          </View>
         </View>
       </Swiper>
     );
